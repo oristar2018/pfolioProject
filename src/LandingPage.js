3d5a525e24f32af53be0c062b379c7b15472f0ad
Oristar2018 <ulricvanstatter@gmail.com>
2018-10-02 23:34:53 +0200

    update

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -40,7 +40,7 @@ class LandingPage extends Component {
     var parallaxInstance = new Parallax(scene, { 
     	relativeInput: true,
     	hoverOnly: true,
-      scalarX: 2.5,
+      scalarX: 0,
       scalarY: 0,
       pointerEvents: true,
 
@@ -49,7 +49,7 @@ class LandingPage extends Component {
      var parallaxInstance2 = new Parallax(scene2, { 
     	relativeInput: true,
     	hoverOnly: true,
-       scalarX: 2.5,
+       scalarX: 0,
       scalarY: 0,
       pointerEvents: true,
 
@@ -60,7 +60,7 @@ class LandingPage extends Component {
     var parallaxInstance3 = new Parallax(sceneParis, {
       relativeInput: true,
       hoverOnly: true,
-       scalarX: 2.5,
+       scalarX: 0,
       scalarY: 0,
       pointerEvents: true,
     });
@@ -69,8 +69,8 @@ class LandingPage extends Component {
     var parallaxInstance4 = new Parallax(foreground, {
       relativeInput: true,
       hoverOnly: true,
-       scalarX: 2.5,
-      scalarY: 1.5,
+       scalarX: 5,
+      scalarY: 5,
       pointerEvents: true,
     });
 
@@ -327,37 +327,40 @@ class LandingPage extends Component {
   }
 
   function TechAnim() {
-           var foreground = document.getElementById('content');
-           var foreground4 = document.getElementById('foreground4');
-           var foreground5 = document.getElementById('foreground5');
-           var foreground6 = document.getElementById('foreground6');
-          var ReactLogo = document.getElementById('reactLogo');
-          
-          var routerLogo = document.getElementById('routerLogo');
-          var reduxLogo = document.getElementById('reduxLogo');
-          var webpackLogo = document.getElementById('webpackLogo');
-          var expressLogo = document.getElementById('expressLogo');
-          var es6Logo = document.getElementById('es6Logo');
-          var d3Logo = document.getElementById('d3Logo');
-          var nodeLogo = document.getElementById('nodeLogo');
-          var expressLogo = document.getElementById('expressLogo');
-          var mongodbLogo = document.getElementById('mongodbLogo');
+           //var foreground = document.getElementById('content');
+var foreground4 = document.getElementById("foreground4");
+var foreground5 = document.getElementById("foreground5");
+var foreground6 = document.getElementById("foreground6");
+var ReactLogo = document.getElementById("reactLogo");
 
-    foreground4.style.animation = "3s linear 1 forwards foreground4";
-      foreground5.style.animation = "3s linear 1 forwards foreground5";
-      foreground6.style.animation = "3s linear 1 forwards foreground6";
-     
-      mongodbLogo.style.animation = "1.5s linear 1 forwards appear3";
-       ReactLogo.style.animation = "1.5s linear 0.25s 1 forwards appear3";
-      routerLogo.style.animation = "1.5s linear 0.5s 1 forwards appear3";
-      reduxLogo.style.animation = "1.5s linear 0.75s 1 forwards appear3";
-      webpackLogo.style.animation = "1.5s linear 1s 1 forwards appear3";
-      expressLogo.style.animation = "1.5s linear 1.25s 1 forwards appear3";
-      es6Logo.style.animation = "1.5s linear 1.5s 1 forwards appear4";
-      d3Logo.style.animation = "1.5s linear 2s 1 forwards appear4";
-      nodeLogo.style.animation = "1.5s linear 1.75s 1 forwards appear4";
+var routerLogo = document.getElementById("routerLogo");
+var reduxLogo = document.getElementById("reduxLogo");
+var webpackLogo = document.getElementById("webpackLogo");
+var expressLogo = document.getElementById("expressLogo");
+var es6Logo = document.getElementById("es6Logo");
+var d3Logo = document.getElementById("d3Logo");
+var nodeLogo = document.getElementById("nodeLogo");
+var expressLogo = document.getElementById("expressLogo");
+var mongodbLogo = document.getElementById("mongodbLogo");
 
-      return 'done'
+
+  foreground4.style.animation = "3s linear 1 forwards foreground4";
+foreground5.style.animation = "3s linear 1 forwards foreground5";
+foreground6.style.animation = "3s linear 1 forwards foreground6";
+
+mongodbLogo.style.animation = "1.5s linear 1 forwards appear3";
+ReactLogo.style.animation = "1.5s linear 0.25s 1 forwards appear3";
+routerLogo.style.animation = "1.5s linear 0.5s 1 forwards appear3";
+reduxLogo.style.animation = "1.5s linear 0.75s 1 forwards appear3";
+webpackLogo.style.animation = "1.5s linear 1s 1 forwards appear3";
+expressLogo.style.animation = "1.5s linear 1.25s 1 forwards appear3";
+es6Logo.style.animation = "1.5s linear 1.5s 1 forwards appear4";
+d3Logo.style.animation = "1.5s linear 2s 1 forwards appear4";
+nodeLogo.style.animation = "1.5s linear 1.75s 1 forwards appear4";
+
+
+
+//return "done";
   }
 
 
@@ -417,26 +420,37 @@ let buttonUpFrameStyle = {
 
     return (
     	<div id="LandingContainer" onTouchMove={this.Touch} onWheel={this.Scroll}>
-    	<div id="scene" style={{background: "#18121E", width: "100vw", overflowX: "hidden"}}>
+    	<div id="scene" style={{background: /*"#18121E"*/ "url('banner.jpg')", width: "100vw", overflowX: "hidden"}}>
   
-  <div data-depth="0.2" id="layer1" style={{background: "#18121E", width: "100vw", overflowX: "hidden"}}></div>
+  <div data-depth="0.2" id="layer1" style={{background:  /*"#18121E"*/ "url('banner.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "100% 100%", width: "100vw", overflowX: "hidden"}}></div>
   <div data-depth="0.6" id="navTextContainer"><p id="navText">Use mousewheel or click/tap buttons to navigate</p></div>
   <div onTouchMove={this.Touch} id="foreground" onWheel={this.Scroll} ref={(div) => {this.Foreground = div}} data-depth="0.9"><h1 id="foreground1">Hello</h1><h1 id="foreground2">I'm Benoit-Henri</h1><h1 id="foreground3">...and i provide Solutions</h1></div>
 </div>
 <div onWheel={this.Scroll} id="buttonDownDiv"><button className="animButton" onClick={() => {scrollToComponent(this.Plain, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'}); TechAnim3()}} style={buttonDownFrameStyle}>Down</button></div>
 <div ref={(div) => {this.Plain = div}} onWheel={this.Scroll} className="Plain" id="plain">
 
-<div id="Background" style={{ height: "90vh", width: "55vw", background: "url('aquarelle.jpg')", backgroundRepeat: "no-repeat ", backgroundSize: "100% 100%"}}></div>
-<div id="BackgroundAnims"  style={{ height: "90vh", width: "50vw"}}><img id="Dove" style={{height: "5vh", width: "5vw"}} alt="Dove" src="Dove.gif"/></div>
-<div id="ParisBackground" data-depth="0.3"><img id="Eiffel" style={TowerStyle} src="EiffelTower.png" alt="Eiffel tower"/>
+<div id="Background" style={{ height: "90vh", width: "100vw", background: "url('rhapsody.jpg')", backgroundRepeat: "no-repeat ", backgroundSize: "100% 100%"}}></div>
+{/*<div id="BackgroundAnims"  style={{ height: "90vh", width: "50vw"}}><img id="Dove" style={{height: "5vh", width: "5vw"}} alt="Dove" src="Dove.gif"/></div>
+<div id ="ParisSublayers4" style={{width: "50vw", height: "90vh"}} data-depth="0.2">
+
+<img style={{width: "50w", height: "40vh", position: "relative", top: "55vh"}} src="forest.png" alt="Tree"/>
+
+
 </div>
+<div id="ParisBackground" data-depth="0.3"><img id="Eiffel" style={TowerStyle} src="EiffelTower.png" alt="Eiffel tower"/>
+</div>*/}
 <div id="OriginContainer" data-depth="0.4"><div id="OriginLeft"></div><div id="OriginRight"><div id="OriginRightUp"><p id="OriginText">I live and work in Paris.</p></div><div id="OriginRightDown"><p id="OriginText2">I also enjoy working abroad. <br/> Switzerland, England, Luxembourg...</p></div></div></div>
-<div id="ParisSublayers2" data-depth="0.5"><img style={{width: "8.5vw", height: "15vh", position: "relative", top: "69vh"}} src="oak.png" alt="Tree" /><img style={{width: "8.5vw", height: "15vh",  position: "relative", top: "69vh"}} src="oak.png" alt="Tree"/><img style={{width: "8.5vw", height: "15vh",  position: "relative", top: "69vh"}} src="oak.png" alt="Tree"/>
+{/*<div id="ParisSublayers2" data-depth="0.5"><img style={{width: "8.5vw", height: "15vh", position: "relative", top: "69vh"}} src="oak.png" alt="Tree" /><img style={{width: "8.5vw", height: "15vh",  position: "relative", top: "69vh"}} src="oak.png" alt="Tree"/><img style={{width: "8.5vw", height: "15vh",  position: "relative", top: "69vh"}} src="oak.png" alt="Tree"/>
 <img style={{width: "12.5vw", height: "20vh", position: "relative", top: "70vh"}} src="oak.png" alt="Tree"/><img style={{width: "8.5vw", height: "15vh",  position: "relative", top: "69vh", left: "-5vw"}} src="oak.png" alt="Tree"/><img style={{width: "8.5vw", height: "15vh",  position: "relative", top: "69vh", left: "-4vw"}} src="oak.png" alt="Tree"/></div>
 <div id="Man" style={{width: "50vw", height: "90vh"}} data-depth="0.6"><img alt="walking man" style={{height: "10vh", width: "6.5vw", position: "relative", top: "70vh", left: "21.5vw"}} id="ManGif" src="WalkBack.gif"/></div>
+<div id ="ParisSublayers3" style={{width: "50vw", height: "90vh"}} data-depth="0.25">
+<img style={{width: "12.5vw", height: "20vh", position: "relative", top: "70vh", left: "3vw"}} src="oak.png" alt="Tree"/>
+<img style={{width: "12.5vw", height: "20vh", position: "relative", top: "70vh", left: "-4vw"}} src="oak.png" alt="Tree"/>
+<img style={{width: "12.5vw", height: "20vh", position: "relative", top: "70vh", left: "-2vw"}} src="oak.png" alt="Tree"/>
+<img style={{width: "12.5vw", height: "20vh", position: "relative", top: "70vh", left: "-4vw"}} src="oak.png" alt="Tree"/></div>
 <div id="ParisSublayers" style={{width: "50vw", height: "90vh"}} data-depth="0.7"><img style={{width: "12.5vw", height: "20vh", position: "relative", top: "70vh"}} src="oak.png" alt="Tree"/><img style={{width: "12.5vw", height: "20vh",  position: "relative", top: "70vh"}} src="oak.png" alt="Tree"/><img style={{width: "12.5vw", height: "20vh",  position: "relative", top: "70vh"}} src="oak.png" alt="Tree"/>
 <img style={{width: "12.5vw", height: "20vh", position: "relative", top: "70vh"}} src="oak.png" alt="Tree"/>
-</div>
+</div>*/}
 <div onTouchMove={this.Touch} ref={(div) => {this.PlainForeground = div}} onWheel={this.Scroll} id="ParisForeground" data-depth="0.8"><button className="animButton" id="ParisButton1" onClick={() => {scrollToComponent(this.Foreground, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'})}} style={buttonUpFrameStyle}>arrow Up</button><button className="animButton" id="ParisButton2" onClick={() => {scrollToComponent(this.ContentForeground, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'}); TechAnim2()}} style={buttonDownFrameStyle}>arrow Down</button></div>
 
 </div>
@@ -446,13 +460,13 @@ let buttonUpFrameStyle = {
 <p>To me, coding is no only a means of implementing an algorithm or solution to a problem but also an artform. Like litteral language translation, the wording and the turn of phrase you employ will greatly affect your reader/interlocutor. </p>
 <p>I always believe in making my code better.</p>*/}
 <div className="contentDiv" data-depth="0.2"><div className="quotes" id="quote1">I am full-stack developper.</div></div>
-<div className="contentDiv" data-depth="0.4"><div className="quotes" id="quote2">I enjoy cracking problems, thinking about the best possible design patterns and maximizing efficiency.</div></div>
-<div className="contentDiv" data-depth="0.6"><div className="quotes" id="quote3">To me, coding is no only a means of implementing an algorithm or solution to a problem but also an artform.</div></div>
-<div className="contentDiv" data-depth="0.7"><div className="quotes" id="quote4">Like litteral language translation, the wording and the turn of phrase you employ will greatly affect your reader/interlocutor.</div> </div>
+<div className="contentDiv" data-depth="0.4"><div className="quotes" id="quote2">I enjoy cracking problems</div></div>
+<div className="contentDiv" data-depth="0.6"><div className="quotes" id="quote3">I value efficiency.</div></div>
+<div className="contentDiv" data-depth="0.7"><div className="quotes" id="quote4">Coding is an artform </div> </div>
 <div className="contentDiv" data-depth="0.8"><div className="quotes" id="quote5">I always believe in making my code better.</div></div>
 
 
-<div  id="contentForeground" data-depth="0.9" ref={(div) => {this.ContentForeground = div}} onWheel={this.Scroll}><button className="animButton" id="contentButton1"  onClick={() => {scrollToComponent(this.PlainForeground, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'})}} style={buttonUpFrameStyle}>up button</button><button className="animButton" id="contentButton2" onClick={() => {scrollToComponent(this.ButtonWorks, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'}); TechAnim()}} style={buttonDownFrameStyle}>down button</button></div>
+<div  id="contentForeground" data-depth="0.9" ref={(div) => {this.ContentForeground = div}} onWheel={this.Scroll}><button className="animButton" id="contentButton1"  onClick={() => {scrollToComponent(this.PlainForeground, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'})}} style={buttonUpFrameStyle}>up button</button><button className="animButton" id="contentButton2" onClick={async (e) => {e.preventDefault(); await scrollToComponent(this.ButtonWorks, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'}); TechAnim()}} style={buttonDownFrameStyle}>down button</button></div>
 
 
 </section>
@@ -472,7 +486,7 @@ let buttonUpFrameStyle = {
   
 
   </div>
-  <div onTouchMove={this.Touch} onWheel={this.Scroll} ref={(div) => {this.Foreground2 = div}} id="Foreground2" data-depth="0.9"><h1 id="foreground4">And here are</h1><h1 id="foreground5">The technologies that I've learnt and love using...everyday</h1><h1 id="foreground6"></h1>
+  <div onTouchMove={this.Touch} onWheel={this.Scroll} ref={(div) => {this.Foreground2 = div}} id="Foreground2" data-depth="0.9"><h1 id="foreground4">And here are</h1><h1 id="foreground5">Some of the technologies I use everyday</h1><h1 id="foreground6"></h1>
   <button id="TechListUpButton" onClick={() => {scrollToComponent(this.Foreground, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'})}} style={buttonUpFrameStyle}>Top</button></div>
 </div>
 
